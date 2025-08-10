8e855ed92df4ac57c07f33b0e1d6d1a99c3bb21c
Kostya <kostantinkorocenko301@gmail.com>
2025-08-10 23:29:38 +0300

    change shown message

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ export class App extends React.Component<{}, AppState> {
     pressedKey: null,
   };
 
-
   handleKeyPress = (event: KeyboardEvent) => {
     this.setState({ pressedKey: event.key });
   };
@@ -27,7 +26,7 @@ export class App extends React.Component<{}, AppState> {
       <div className="App">
         <p className="App__message">
           {this.state.pressedKey
-            ? 'The last pressed key is ' + this.state.pressedKey
+            ? `The last pressed key is [${this.state.pressedKey}]`
             : 'Nothing was pressed yet'}
         </p>
       </div>
